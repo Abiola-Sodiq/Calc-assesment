d514106ab9795b0c8a92c3f6454101982817bdce
abiola-sodiq <rasababe01@gmail.com>
2023-07-26 14:53:10 +0100

    first commit

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -21,26 +21,28 @@ const Calculator = () => {
 
   return (
 <div className=" flex justify-center items-center h-[100svh] w-screen">
-<div className=" w-[40%] h-[50%] bg-slate-500">
-      <input type="text" value={displayValue} readOnly className=" w-[70%] h-9 flex items-center " />
-      <div className=" ">
-        <button onClick={() => clearDisplay()}>C</button>
-        <button onClick={() => appendToDisplay("/")}>/</button>
-        <button onClick={() => appendToDisplay("*")}>*</button>
-        <button onClick={() => appendToDisplay("7")}>7</button>
-        <button onClick={() => appendToDisplay("8")}>8</button>
-        <button onClick={() => appendToDisplay("9")}>9</button>
-        <button onClick={() => appendToDisplay("-")}>-</button>
-        <button onClick={() => appendToDisplay("4")}>4</button>
-        <button onClick={() => appendToDisplay("5")}>5</button>
-        <button onClick={() => appendToDisplay("6")}>6</button>
-        <button onClick={() => appendToDisplay("+")}>+</button>
-        <button onClick={() => appendToDisplay("1")}>1</button>
-        <button onClick={() => appendToDisplay("2")}>2</button>
-        <button onClick={() => appendToDisplay("3")}>3</button>
-        <button onClick={() => calculate()}>=</button>
-        <button onClick={() => appendToDisplay("0")}>0</button>
-        <button onClick={() => appendToDisplay(".")}>.</button>
+  <div className=" p-12 h-[50%] bg-slate-500">
+     <div className=" flex justify-center items-center mt-8">
+     <input type="text" value={displayValue} readOnly className=" w-[70%] h-9 flex items-center " />
+     </div>
+      <div className=" grid grid-cols-[1fr_1fr_1fr_1fr]">
+        <button onClick={() => clearDisplay()} className=" w-20 h-10 border ">C</button>
+        <button onClick={() => appendToDisplay("0")} className=" w-20 h-10 border ">0</button>
+        <button onClick={() => appendToDisplay("1")} className=" w-20 h-10 border ">1</button>
+        <button onClick={() => appendToDisplay("2")} className=" w-20 h-10 border ">2</button>
+        <button onClick={() => appendToDisplay("3")} className=" w-20 h-10 border ">3</button>
+        <button onClick={() => appendToDisplay("4")} className=" w-20 h-10 border ">4</button>
+        <button onClick={() => appendToDisplay("5")} className=" w-20 h-10 border ">5</button>
+        <button onClick={() => appendToDisplay("7")} className=" w-20 h-10 border ">7</button>
+        <button onClick={() => appendToDisplay("6")} className=" w-20 h-10 border ">6</button>
+        <button onClick={() => appendToDisplay("8")} className=" w-20 h-10 border ">8</button>
+        <button onClick={() => appendToDisplay("9")} className=" w-20 h-10 border ">9</button>
+        <button onClick={() => appendToDisplay("-")} className=" w-20 h-10 border ">-</button>
+        <button onClick={() => appendToDisplay("+")} className=" w-20 h-10 border ">+</button>
+        <button onClick={() => appendToDisplay("*")} className=" w-20 h-10 border ">*</button>
+        <button onClick={() => appendToDisplay("/")} className=" w-20 h-10 border ">/</button>
+        <button onClick={() => appendToDisplay(".")} className=" w-20 h-10 border ">.</button>
+        <button onClick={() => calculate()} className=" w-20 h-10 border ">=</button>
       </div>
     </div>
 </div>
